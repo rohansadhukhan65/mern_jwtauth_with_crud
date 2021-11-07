e7f1fdb74c5eb9f578f81deef5fee47c61a0979d
rohansadhukhan65 <sadhukhanrohan15@gmail.com>
2021-11-07 15:47:50 +0530

    yo

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router ,  Routes, Route } from "react-router-dom";
+
+// import Components/Screens ....
+import Login from './Screens/Login';
+import Signup from './Screens/Signup';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={Login} />
+        <Route path="/register" element={Signup} />
+      </Routes>
+    </Router>
   );
 }
 
